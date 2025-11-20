ae2dbed9ccf34d576ec5cc2c0081eec75ccfbcb7
zk657 <zk5454721@gmail.com>
2025-11-20 17:20:17 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://usermanagment-1.onrender.com",
                 methods: ["GET", "POST", "PUT", "DELETE"],
         credentials:true
     })
